09cf30fe6a22e9480f8f80ae1a714cac3e9d1233
donggyu <donggyu904@gmail.com>
2023-02-01 14:25:14 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TiktokApp());
+}
+
+class TiktokApp extends StatelessWidget {
+  const TiktokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tiktok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(),
+    );
+  }
+}
